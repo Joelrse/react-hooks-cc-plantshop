c2408de08f273e3375b73756dcb4dedd4a659608
Joel <rapp.joel20@gmail.com>
2024-09-10 14:09:31 -0400

    lab complete

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,22 @@
-import React from "react";
-import PlantCard from "./PlantCard";
 
-function PlantList() {
+import PlantCard from "./PlantCard";
+ 
+
+function PlantList({ plants }) {
+
+  
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard
+          key={plant.id}
+          image={plant.image}
+          name={plant.name}
+          price={plant.price}
+          inStock={plant.inStock}
+        />
+      ))}
+    </ul>
   );
 }
 
